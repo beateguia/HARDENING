847ac2b0e0dd5c4bf9c1726fcc4819aee8297920
Alma <beateguia@gmail.com>
2020-09-04 10:38:32 -0400

    Project 12 answers

--- CentosHardening.sh
+++ CentosHardening.sh
@@ -1,13 +1,12 @@
-#!bin/bash
+#!/bin/bash
 #Author: Bea T
-#Date 9/1/2020
+#Date: 9/1/20
+#Description: CIS parameters for securing the Centos7 server.
 
-#Description: Cis Parameters for Securing Centos7
-
-# Disable mounting of unneeded filesystems CIS 1.1.1 and CIS 3.5
+#Disable mounting of unneeded filesystems CIS 1.1.1 and CIS 3.5
 
 cat << EOF >> /etc/modprobe.d/CIS.conf
-echo install cramfs /bin/true
+install cramfs /bin/true
 install freevxfs /bin/true
 install jffs2 /bin/true
 install hfs /bin/true
@@ -18,3 +17,4 @@ install dccp /bin/true
 install sctp /bin/true
 install rds /bin/true
 install tipc /bin/true
+EOF
